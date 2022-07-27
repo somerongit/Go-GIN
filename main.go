519376c272b77053911d2dd7facb9262c9b3621c
somerongit <someron.bakuli05@gmail.com>
2022-07-27 09:21:37 +0530

    Get post with auth header

--- main.go
+++ main.go
@@ -18,5 +18,39 @@ func main() {
 		ctx.String(http.StatusOK, "pong")
 	})
 
+	/*
+	 Accessing URL Params and Query Params
+	 Return empty string if query not exsits
+	*/
+	routerGroup.GET("/users/:id", func(ctx *gin.Context) {
+		// id := ctx.Param("id")
+		// Or
+		id := ctx.Params.ByName("id")
+		info := ""
+
+		if ctx.Query("isAddress") == "true" {
+			info = info + ",\n\tPincode: 101010,\n\tCity: Kolkata"
+		}
+		if ctx.Query("isAdmin") == "true" {
+			info = info + ",\n\tIsAdmin: true"
+		}
+
+		ctx.String(http.StatusOK, "{\n\tId: 0"+id+",\n\tName: S.Bakuli"+info+"\n}")
+	})
+
+	// Accessing Post data and header info
+	routerGroup.POST("/users", func(ctx *gin.Context) {
+		if ctx.GetHeader("X-Secure-Host") == "3000" {
+			name := ctx.PostForm("name")
+			pincode := ctx.PostForm("pincode")
+			city := ctx.PostForm("city")
+			isAdmin := ctx.DefaultPostForm("isAdmin", "false")
+
+			ctx.String(http.StatusAccepted, "{\n\tId: 01,\n\tName: "+name+",\n\tPincode: "+pincode+",\n\tCity: "+city+",\n\tIsAdmin: "+isAdmin+"\n}")
+		} else {
+			ctx.String(http.StatusForbidden, "You are not authorized...")
+		}
+	})
+
 	log.Fatal(router.Run(port))
 }
